7ed446055240be906b2d7cfa3f4c00a6a4725d9d
Vlad Agapov <homa_1998@mail.ru>
2020-02-20 12:38:00 +0600

    Map app

---
+++ MapApp2/MapApp2/MapApp2/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Essentials;
+
+namespace MapApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        public static string temp;
+        public static string x = "";
+        private int count;
+        public void InputStart()
+        {
+            for (int i = 1; i < App.Current.Properties.Count + 1; i++)
+                picker.Items.Add(App.Current.Properties[i.ToString()].ToString());
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+            InputStart();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            picker.Items.Clear();
+            picker.Items.Add(entry1.Text + ';' + entry2.Text);
+            count = App.Current.Properties.Count + 1;
+            App.Current.Properties.Add(count.ToString(), entry1.Text + ';' + entry2.Text);
+        }
+
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            temp = picker.SelectedItem.ToString();
+            x = "";
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i] == ';') break;
+                else x += temp[i];
+            }
+            temp = temp.Remove(0, x.Length + 1);
+            Navigation.PushModalAsync(new mapShow());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            count = 0;
+            App.Current.Properties.Clear();
+            picker.Items.Clear();
+        }
+    }
+}
---
+++ MapApp2/MapApp2/MapApp2/mapShow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Maps;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MapApp2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class mapShow : ContentPage
+	{
+        Xamarin.Forms.Maps.Map map = new Xamarin.Forms.Maps.Map(MapSpan.FromCenterAndRadius(new Position(Convert.ToDouble(MainPage.x), Convert.ToDouble(MainPage.temp)), Distance.FromMiles(0.3)));
+        Pin point;
+        List<Pin> Pins = new List<Pin>();
+        public mapShow()
+		{
+            //54.941926699999996, 73.40059526854125
+            InitializeComponent();
+            ts.Children.Add(map);
+            Content = ts;
+            geoFunc(Convert.ToDouble(MainPage.x), Convert.ToDouble(MainPage.temp));
+            
+        }
+        public void geoFunc(double x, double y)
+        {
+            point = new Pin()
+            {
+                Label = "Hello there",
+                Position = new Position(x, y)
+            };
+            Pins.Add(point);
+        }
+        public void geoFunc2(double x, double y)
+        {
+            point = new Pin()
+            {
+                Label = "Hello there",
+                Position = new Position(x, y)
+            };
+            Pins.Add(point);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new MainPage());
+        }
+    }
+}
